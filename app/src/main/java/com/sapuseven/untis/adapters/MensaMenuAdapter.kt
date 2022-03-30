a2741a9ff30f924d84fbbabc32eaff22b072a970
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-03-30 13:29:09 +0200

    Add food

---
+++ app/src/main/java/com/sapuseven/untis/adapters/MensaMenuAdapter.kt
@@ -0,0 +1,35 @@
+package com.sapuseven.untis.adapters
+
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sapuseven.untis.R
+import com.sapuseven.untis.data.mensa.ListItem
+
+class MensaMenuAdapter(
+	private val menu: ArrayList<ListItem>
+) : RecyclerView.Adapter<MensaMenuAdapter.ViewHolder>() {
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val v = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
+		return ViewHolder(v)
+	}
+
+	override fun getItemCount(): Int = menu.size
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		val message = menu[position]
+
+		holder.tvSubject.text = message.title
+		holder.tvBody.text = message.summary
+		holder.tvBody.movementMethod = LinkMovementMethod.getInstance()
+	}
+
+	class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
+		val tvSubject: TextView = rootView.findViewById(R.id.textview_itemmessage_subject)
+		val tvBody: TextView = rootView.findViewById(R.id.textview_itemmessage_body)
+	}
+}
---
+++ app/src/main/java/com/sapuseven/untis/data/mensa/ListItem.kt
@@ -0,0 +1,6 @@
+package com.sapuseven.untis.data.mensa
+
+data class ListItem(
+	val title: String,
+	val summary: String
+)
