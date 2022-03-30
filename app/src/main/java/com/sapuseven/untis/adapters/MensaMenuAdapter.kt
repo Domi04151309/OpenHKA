900b3fc4ffc02e193c94b5cd45cad6c1fac9416d
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-03-30 16:59:48 +0200

    Start navigation on click

--- app/src/main/java/com/sapuseven/untis/adapters/MensaMenuAdapter.kt
+++ app/src/main/java/com/sapuseven/untis/adapters/MensaMenuAdapter.kt
@@ -10,22 +10,28 @@ import com.sapuseven.untis.R
 import com.sapuseven.untis.data.mensa.ListItem
 
 class MensaMenuAdapter(
-	private val menu: ArrayList<ListItem>
+	private val items: ArrayList<ListItem>
 ) : RecyclerView.Adapter<MensaMenuAdapter.ViewHolder>() {
+
+	var onClickListener: View.OnClickListener? = null
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 		val v = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
 		return ViewHolder(v)
 	}
 
-	override fun getItemCount(): Int = menu.size
+	override fun getItemCount(): Int = items.size
 
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-		val message = menu[position]
+		val message = items[position]
+
+		holder.itemView.setOnClickListener(onClickListener)
 
 		holder.tvSubject.text = message.title
 		holder.tvBody.text = message.summary
 		holder.tvBody.movementMethod = LinkMovementMethod.getInstance()
+
+		holder.tvBody.visibility = if (message.summary.isEmpty()) View.GONE else View.VISIBLE
 	}
 
 	class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
