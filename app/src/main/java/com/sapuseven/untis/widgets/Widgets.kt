9bdb8f6ee51f0d4ed087890140ad0f54ad4f8d0c
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-04-11 22:24:47 +0200

    Different widget titles

--- app/src/main/java/com/sapuseven/untis/widgets/Widgets.kt
+++ app/src/main/java/com/sapuseven/untis/widgets/Widgets.kt
@@ -1,13 +1,18 @@
 package com.sapuseven.untis.widgets
 
+import com.sapuseven.untis.R
+
 class MessagesWidget : BaseWidget() {
 	override fun getWidgetType(): Int = WidgetRemoteViewsFactory.WIDGET_TYPE_MESSAGES
+	override fun getTitleInt(): Int = R.string.widget_daily_messages
 }
 
 class TimetableWidget : BaseWidget() {
 	override fun getWidgetType(): Int = WidgetRemoteViewsFactory.WIDGET_TYPE_TIMETABLE
+	override fun getTitleInt(): Int = R.string.widget_timetable
 }
 
 class MensaWidget : BaseWidget() {
 	override fun getWidgetType(): Int = WidgetRemoteViewsFactory.WIDGET_TYPE_MENSA
+	override fun getTitleInt(): Int = R.string.widget_mensa
 }
