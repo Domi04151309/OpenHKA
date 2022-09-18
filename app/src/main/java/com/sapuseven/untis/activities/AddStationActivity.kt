51456472b2b01dc6678e96e1c78db3454d5ffd0a
Dominik <36272047+Domi04151309@users.noreply.github.com>
2022-09-18 23:48:17 +0200

    Add stop search

--- app/src/main/java/com/sapuseven/untis/activities/AddStationActivity.kt
+++ app/src/main/java/com/sapuseven/untis/activities/AddStationActivity.kt
@@ -3,9 +3,11 @@ package com.sapuseven.untis.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -16,11 +18,13 @@ import com.sapuseven.untis.helpers.strings.StringLoader
 import com.sapuseven.untis.interfaces.StringDisplay
 import org.json.JSONArray
 import org.json.JSONObject
+import java.lang.Integer.min
 import java.lang.ref.WeakReference
+import java.net.URLEncoder
 
 class AddStationActivity : BaseActivity(), StringDisplay {
-	private val stationList = arrayListOf<ListItem>()
-	private val stationAdapter = MessageAdapter(stationList)
+	private val list = arrayListOf<ListItem>()
+	private val adapter = MessageAdapter(list)
 	private var stationsLoading = true
 	private val keyMap: MutableMap<String, String> = mutableMapOf()
 	private lateinit var stringLoader: StringLoader
@@ -28,7 +32,9 @@ class AddStationActivity : BaseActivity(), StringDisplay {
 	private lateinit var swiperefreshlayout: SwipeRefreshLayout
 
 	companion object {
-		private const val API_URL: String = "https://www.kvv.de/tunnelEfaDirect.php?outputFormat=JSON&coordOutputFormat=WGS84[dd.ddddd]&"
+		// The 0 is there to prevent auto stop selection by the API
+		private const val API_URL: String =
+			"https://www.kvv.de/tunnelEfaDirect.php?outputFormat=JSON&coordOutputFormat=WGS84[dd.ddddd]&action=XSLT_STOPFINDER_REQUEST&type_sf=any&name_sf=0"
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,20 +44,21 @@ class AddStationActivity : BaseActivity(), StringDisplay {
 		stringLoader = StringLoader(
 			WeakReference(this),
 			this,
-			"${API_URL}action=XSLT_STOPFINDER_REQUEST&name_sf=Kunstakademie/Hochschule&type_sf=any"
+			API_URL + "Kunstakademie/Hochschule"
 		)
 		recyclerview = findViewById(R.id.recyclerview_infocenter)
 		swiperefreshlayout = findViewById(R.id.swiperefreshlayout_infocenter)
 
 		recyclerview.layoutManager = LinearLayoutManager(this)
-		recyclerview.adapter = stationAdapter
+		recyclerview.adapter = adapter
 		swiperefreshlayout.isRefreshing = stationsLoading
 		swiperefreshlayout.setOnRefreshListener { refreshStations(StringLoader.FLAG_LOAD_SERVER) }
 
 		refreshStations(StringLoader.FLAG_LOAD_CACHE)
 
-		stationAdapter.onClickListener = View.OnClickListener {
-			val key = it.findViewById<TextView>(R.id.textview_itemmessage_subject).text.toString()
+		adapter.onClickListener = View.OnClickListener {
+			val key = it.findViewById<TextView>(R.id.textview_itemmessage_subject).text.toString() +
+					it.findViewById<TextView>(R.id.textview_itemmessage_body).text.toString()
 			if (key.isNotEmpty()) {
 				val mapIntent = Intent(
 					Intent.ACTION_VIEW, Uri.parse(
@@ -64,13 +71,37 @@ class AddStationActivity : BaseActivity(), StringDisplay {
 		}
 	}
 
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		menuInflater.inflate(R.menu.activity_people_menu, menu)
+		(menu.findItem(R.id.search).actionView as SearchView)
+			.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+				override fun onQueryTextSubmit(query: String): Boolean {
+					return onQueryTextChange(query)
+				}
+
+				override fun onQueryTextChange(newText: String): Boolean {
+					if (newText.trim().isEmpty()) return true
+					stringLoader = StringLoader(
+						WeakReference(this@AddStationActivity),
+						this@AddStationActivity,
+						API_URL + URLEncoder.encode(newText, "UTF-8")
+					)
+					stationsLoading = true
+					swiperefreshlayout.isRefreshing = true
+					refreshStations(StringLoader.FLAG_LOAD_SERVER)
+					return true
+				}
+			})
+		return true
+	}
+
 	private fun refreshStations(flags: Int) {
 		stationsLoading = true
 		stringLoader.load(flags)
 	}
 
 	override fun onStringLoaded(string: String) {
-		stationList.clear()
+		list.clear()
 		val json = (JSONObject(string).optJSONObject("stopFinder") ?: JSONObject()).run {
 			optJSONArray("points")
 				?: JSONArray().put(
@@ -79,24 +110,26 @@ class AddStationActivity : BaseActivity(), StringDisplay {
 		}
 		var currentItem: JSONObject
 		var refs: JSONObject
-		for (i in 0 until json.length()) {
+		for (i in 0 until min(json.length(), 20)) {
 			currentItem = json.optJSONObject(i)
+			if (currentItem.optString("anyType") != "stop") continue
+
 			refs = (currentItem.optJSONObject("ref") ?: JSONObject())
-			stationList.add(
+			list.add(
 				ListItem(
-					currentItem.optString("name"),
-					refs.optString("id")
+					currentItem.optString("object"),
+					currentItem.optString("mainLoc")
 				)
 			)
-			keyMap[currentItem.optString("name")] = refs.optString("coords")
-				.split(",").toMutableList().apply {
+			keyMap[currentItem.optString("object") + currentItem.optString("mainLoc")] =
+				refs.optString("coords").split(",").toMutableList().apply {
 					if (size < 2) return
 					val temp = this[1]
 					this[1] = this[0]
 					this[0] = temp
 				}.joinToString(",")
 		}
-		stationAdapter.notifyDataSetChanged()
+		adapter.notifyDataSetChanged()
 		stationsLoading = false
 		swiperefreshlayout.isRefreshing = false
 	}
