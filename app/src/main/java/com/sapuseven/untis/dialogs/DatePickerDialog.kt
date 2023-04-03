5c929197fe34db414b40904e125480642d9f79d9
domi04151309 <dominikworldlp@gmail.com>
2023-04-03 18:49:40 +0200

    Change date picker layout

--- app/src/main/java/com/sapuseven/untis/dialogs/DatePickerDialog.kt
+++ app/src/main/java/com/sapuseven/untis/dialogs/DatePickerDialog.kt
@@ -2,7 +2,9 @@ package com.sapuseven.untis.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.sapuseven.untis.R
 import org.joda.time.DateTime
 
@@ -10,18 +12,54 @@ class DatePickerDialog : DialogFragment() {
 	var dateSetListener: android.app.DatePickerDialog.OnDateSetListener? = null
 
 	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-		val dialog: android.app.DatePickerDialog = arguments?.let {
-			val year = it.getInt("year")
-			val month = it.getInt("month")
-			val day = it.getInt("day")
-			android.app.DatePickerDialog(requireContext(), dateSetListener, year, month - 1, day)
+		val datePicker = DatePicker(requireContext())
+		arguments?.let {
+			datePicker.updateDate(
+				it.getInt("year"),
+				it.getInt("month") - 1,
+				it.getInt("day")
+			)
 		} ?: run {
 			val now = DateTime.now()
-			android.app.DatePickerDialog(requireContext(), dateSetListener, now.year, now.monthOfYear - 1, now.dayOfMonth)
+			datePicker.updateDate(
+				now.year,
+				now.monthOfYear - 1,
+				now.dayOfMonth
+			)
 		}
-		dialog.setButton(android.app.DatePickerDialog.BUTTON_NEUTRAL, getString(R.string.all_dialog_datepicker_button_today)) { _, _ ->
+
+		val dialog = MaterialAlertDialogBuilder(requireContext())
+			.setView(datePicker)
+			.create()
+
+		dialog.setButton(
+			android.app.DatePickerDialog.BUTTON_POSITIVE,
+			getString(R.string.all_ok)
+		) { _, _ ->
+			dateSetListener?.onDateSet(
+				datePicker,
+				datePicker.year,
+				datePicker.month,
+				datePicker.dayOfMonth
+			)
+		}
+
+		dialog.setButton(
+			android.app.DatePickerDialog.BUTTON_NEGATIVE,
+			getString(R.string.all_cancel)
+		) { _, _ -> }
+
+		dialog.setButton(
+			android.app.DatePickerDialog.BUTTON_NEUTRAL,
+			getString(R.string.all_dialog_datepicker_button_today)
+		) { _, _ ->
 			val dateTime = DateTime.now()
-			dateSetListener?.onDateSet(dialog.datePicker, dateTime.year, dateTime.monthOfYear - 1, dateTime.dayOfMonth)
+			dateSetListener?.onDateSet(
+				datePicker,
+				dateTime.year,
+				dateTime.monthOfYear - 1,
+				dateTime.dayOfMonth
+			)
 		}
 		return dialog
 	}
